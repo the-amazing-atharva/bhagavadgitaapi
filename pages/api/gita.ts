cbab29d616f6caee66a25b6294c143904baa67a5
ZZCWC <85448514+losier@users.noreply.github.com>
2022-12-28 20:43:09 +0530

    removed unused elements

--- pages/api/gita.ts
+++ pages/api/gita.ts
@@ -1,11 +1,6 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import fs from "fs";
 import path from "path";
-
-type Data = {
-  name: Data;
-};
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const gitaPath = path.join(process.cwd(), "assets/gita.json");
