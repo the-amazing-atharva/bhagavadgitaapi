38d56b74055f3307fa21db7e59a9d2b45d27ac84
ZZCWC <85448514+losier@users.noreply.github.com>
2023-01-02 10:18:39 +0530

    added gome container

---
+++ components/HomeContainer.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "../styles/homeContainer.module.css";
+
+const HomeContainer = () => {
+  return <div className={styles.home_container}>HomeContainer</div>;
+};
+
+export default HomeContainer;
