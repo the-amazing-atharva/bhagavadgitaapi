f008e05dfad6ea5a073e608c1e45a8e17a9758dd
ZZCWC <85448514+losier@users.noreply.github.com>
2023-01-03 22:44:18 +0530

    basic css

--- components/HomeContainer.tsx
+++ components/HomeContainer.tsx
@@ -1,8 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+import Link from "next/link";
 import styles from "../styles/homeContainer.module.css";
 
 const HomeContainer = () => {
-  return <div className={styles.home_container}>HomeContainer</div>;
+  const [shlok, setShlok] = useState(
+    "धृतराष्ट्र उवाच | धर्मक्षेत्रे कुरुक्षेत्रे समवेता युयुत्सवः |\n मामकाः पाण्डवाश्चैव किमकुर्वत सञ्जय ||१-१||"
+  );
+
+  const [pronc, setPronc] = useState(
+    "dhṛtarāṣṭra uvāca . dharmakṣetre kurukṣetre samavetā yuyutsavaḥ . māmakāḥ pāṇḍavāścaiva kimakurvata sañjaya ||1-1||"
+  );
+
+  const [shlokMeaning, setShlokMeaning] = useState(
+    "1.1. Dhrtarastra said O Sanjaya, what did my sons (and others) and Pandu's sons (and others) actually do when, eager for battle, they assembled on the sacred field, the Kuruksetra (Field of the Kurus)?"
+  );
+
+  return (
+    <div className={`${styles.home_container} ${styles.container}`}>
+      <div className={styles.container_box}>
+        <div className={styles.contentBox}>
+          <h1 className={styles.shlok}>{shlok}</h1>
+          <p className={styles.pronc}>{pronc}</p>
+          <h2 className={styles.shlokMeaning}>{shlokMeaning}</h2>
+        </div>
+        <div className={styles.buttonBox}>
+          <Link className={styles.btn} onClick={() => {}} href="#">
+            Btn1
+          </Link>
+          <Link className={styles.btn} onClick={() => {}} href="#">
+            Btn2
+          </Link>
+          <Link className={styles.btn} onClick={() => {}} href="#">
+            Btn3
+          </Link>
+          <Link className={styles.btn} onClick={() => {}} href="#">
+            Btn4
+          </Link>
+          <Link className={styles.btn} onClick={() => {}} href="#">
+            Btn5
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HomeContainer;
