38fd601ec0e3d0e33c12da4faafd44533117bfde
ZZCWC <85448514+losier@users.noreply.github.com>
2023-01-04 13:05:02 +0530

    added home container with functions

--- components/HomeContainer.tsx
+++ components/HomeContainer.tsx
@@ -1,8 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import styles from "../styles/homeContainer.module.css";
 
 const HomeContainer = () => {
+  const [verseNumber, setVerseNumber] = useState(1);
+  const [verse, setVerse] = useState(1);
+  const [chapter, setChapter] = useState(1);
+  const [chaptherName, setChapterName] = useState("Arjuna Visada Yoga");
   const [shlok, setShlok] = useState(
     "धृतराष्ट्र उवाच |\nधर्मक्षेत्रे कुरुक्षेत्रे समवेता युयुत्सवः |\nमामकाः पाण्डवाश्चैव किमकुर्वत सञ्जय ||१-१||"
   );
@@ -15,6 +19,47 @@ const HomeContainer = () => {
     "1.1. Dhrtarastra said  O Sanjaya, what did my sons (and others) and Pandu's sons (and others) actually do when, eager for battle, they assembled on the sacred field, the Kuruksetra (Field of the Kurus)?"
   );
 
+  const getVerse = async () => {
+    await fetch(`/api/shlok?id=${verseNumber}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setVerse(data.verseNumber);
+        setChapter(data.chapterNumber);
+        setChapterName(data.chapterName);
+        setShlok(data.shlok);
+        setPronc(data.transliteration);
+        setShlokMeaning(data.meaning);
+      })
+      .catch((err) => {
+        alert(
+          "Something went wrong...\nCheck console for more info and please report this issue on github using the link in the footer or contact page."
+        );
+        console.log(err);
+      });
+  };
+
+  const nextShlok = async () => {
+    if (verseNumber <= 700) {
+      setVerseNumber(verseNumber + 1);
+      getVerse();
+    } else {
+      alert("No more shlokas");
+    }
+  };
+  const prevShlok = async () => {
+    if (verseNumber > 0) {
+      setVerseNumber(verseNumber - 1);
+      getVerse();
+    } else {
+      alert("No more shlokas");
+    }
+  };
+  const randomShlok = () => {
+    const randomVerse = Math.floor(Math.random() * 700) + 1;
+    setVerseNumber(randomVerse);
+    getVerse();
+  };
+
   return (
     <div className={`${styles.home_container} ${styles.container}`}>
       <div className={styles.container_box}>
@@ -22,22 +67,18 @@ const HomeContainer = () => {
           <h1 className={styles.shlok}>{shlok}</h1>
           <p className={styles.pronc}>{pronc}</p>
           <h2 className={styles.shlokMeaning}>{shlokMeaning}</h2>
+
+          <code>{`|| ${chaptherName} | Chapter: ${chapter}, Shlok: ${verse} ||`}</code>
         </div>
         <div className={styles.buttonBox}>
-          <Link className={styles.btn} onClick={() => {}} href="#">
-            Previous Chapter
-          </Link>
-          <Link className={styles.btn} onClick={() => {}} href="#">
+          <Link className={styles.btn} onClick={prevShlok} href="#">
             Previous Shlok
           </Link>
-          <Link className={styles.btn} onClick={() => {}} href="#">
+          <Link className={styles.btn} onClick={randomShlok} href="#">
             Random
           </Link>
-          <Link className={styles.btn} onClick={() => {}} href="#">
+          <Link className={styles.btn} onClick={nextShlok} href="#">
             Next Shlok
-          </Link>
-          <Link className={styles.btn} onClick={() => {}} href="#">
-            Next Chapter
           </Link>
         </div>
       </div>
--- pages/api/shlok.ts
+++ pages/api/shlok.ts
@@ -1,7 +1,9 @@
 import { NextApiRequest, NextApiResponse } from "next";
+
 interface Data {
   verseNumber: number;
   chapterNumber: number;
+  chapterName: string;
   shlok: string;
   meaning: string;
   transliteration: string;
@@ -10,6 +12,10 @@ interface Data {
 interface Error {
   errorCode: number;
   errorMessage: string;
+}
+
+interface Dictionary<T> {
+  [key: string]: T;
 }
 
 const getChapterAndVerse = (id: any) => {
@@ -28,6 +34,27 @@ const getChapterAndVerse = (id: any) => {
     }
   }
   return { chapter, verse };
+};
+
+const chapterNameDict: Dictionary<string> = {
+  1: "Arjuna Visada Yoga",
+  2: "Sankhya Yoga",
+  3: "Karma Yoga",
+  4: "Jnana Karma Sanyasa Yoga",
+  5: "Karma Sanyasa Yoga",
+  6: "Dhyana Yoga",
+  7: "Gyaan Vigyana Yoga",
+  8: "Akshara Brahma Yoga",
+  9: "Raja-Vidya-Raja-Guhya Yoga",
+  10: "Vibhuti Yoga",
+  11: "Viswarupa-Darsana Yoga",
+  12: "Bhakti Yoga",
+  13: "Ksetra-Ksetrajna-Vibhaga Yoga",
+  14: "Gunatraya-Vibhaga Yoga",
+  15: "Purushottama Yoga",
+  16: "Daivasura-Sampad-Vibhaga Yoga",
+  17: "Shraddhatraya-Vibhaga Yoga",
+  18: "Moksha-Sanyasa Yoga",
 };
 
 export default async function handler(
@@ -54,9 +81,11 @@ export default async function handler(
     });
   } else {
     const fetchedData = await checkData.json();
+
     res.status(200).json({
       verseNumber: fetchedData.verse,
       chapterNumber: fetchedData.chapter,
+      chapterName: `${chapterNameDict[fetchedData.chapter]}`,
       shlok: `${fetchedData.slok}`,
       meaning: `${fetchedData.gambir.et}`,
       transliteration: `${fetchedData.transliteration}`,
