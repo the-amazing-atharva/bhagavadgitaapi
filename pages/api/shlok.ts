5e60efe2e4bcc7cf2d4b2f2c37dcce43fb1ab62a
ZZCWC <85448514+losier@users.noreply.github.com>
2023-01-04 12:25:04 +0530

    added shlok end point

--- pages/api/shlok.ts
+++ pages/api/shlok.ts
@@ -12,14 +12,40 @@ interface Error {
   errorMessage: string;
 }
 
+const getChapterAndVerse = (id: any) => {
+  const verseCount = [
+    47, 72, 43, 42, 29, 47, 30, 28, 34, 42, 55, 20, 34, 27, 20, 24, 28, 78,
+  ];
+  let chapter = 0;
+  let verse = 0;
+  for (let i = 0; i < verseCount.length; i++) {
+    if (id > verseCount[i]) {
+      id -= verseCount[i];
+    } else {
+      chapter = i + 1;
+      verse = id;
+      break;
+    }
+  }
+  return { chapter, verse };
+};
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data | Error>
 ) {
-  const { id } = req.query;
-  const [ch, sl] = id.split("-"); // will complete soon
+  const { id: number } = req.query;
+  if (typeof number !== "string" || number === undefined || number === "") {
+    res.status(400).json({
+      errorCode: 400,
+      errorMessage: "Bad Request",
+    });
+  }
+  const { chapter, verse } = getChapterAndVerse(number);
 
-  const checkData = await fetch(`https://bhagavadgitaapi.in/slok/${ch}/${sl}`);
+  const checkData = await fetch(
+    `https://bhagavadgitaapi.in/slok/${chapter}/${verse}`
+  );
 
   if (checkData.status === 404) {
     res.status(404).json({
