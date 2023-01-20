7e1e4d10d5d922d63a32b2b6347ff66a48d0b6bb
ZZCWC <85448514+losier@users.noreply.github.com>
2023-01-21 02:51:26 +0530

    added basic contact page

---
+++ components/ContactContainer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "../styles/contact.module.css";
+
+const ContactContainer = () => {
+  return (
+    <div className={styles.contact_container}>
+      <div className={styles.title}>
+        <h1>Contact</h1>
+      </div>
+    </div>
+  );
+};
+
+export default ContactContainer;
---
+++ pages/contact/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../../components/Navbar";
+import ContactContainer from "./ContactContainer";
+
+const index = () => {
+  return (
+    <div>
+      <Navbar />
+      <ContactContainer />
+    </div>
+  );
+};
+
+export default index;
