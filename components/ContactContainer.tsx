923767465130ad8b15054ba12714de01db749120
ZZCWC <85448514+losier@users.noreply.github.com>
2023-01-23 02:00:27 +0530

    added help page

--- components/ContactContainer.tsx
+++ components/ContactContainer.tsx
@@ -13,53 +13,57 @@ const ContactContainer = () => {
       <div className={styles.title}>Contact Us</div>
 
       <div className={styles.helpItem}>
-        <article className="contactCard">
-          <Image
-            src={documentationLogo}
-            alt="Docs logo"
-            width={50}
-            height={50}
-            className={styles.logo}
-          />
-          <h1 className={styles.helpTitle}>Documentation</h1>
-          <p className={styles.helpText}>Need help with with api?</p>
-          <Link href="./documentation" className={styles.gotoBtn} passHref>
-            Explore <span className={styles.arrow}>&#8594;</span>
-          </Link>
+        <article className={styles.contactCard}>
+          <div className={styles.imageDiv}>
+            <Image
+              src={documentationLogo}
+              alt="Docs logo"
+              className={styles.logo}
+            />
+          </div>
+          <div className={styles.content}>
+            <h1 className={styles.helpTitle}>Documentation</h1>
+            <p className={styles.helpText}>Need help with with api?</p>
+            <Link href="./documentation" className={styles.gotoBtn} passHref>
+              Explore <span className={styles.arrow}>&#8594;</span>
+            </Link>
+          </div>
         </article>
-        <article className="contactCard">
-          <Image
-            src={contactLogo}
-            alt="Contact Logo"
-            width={50}
-            height={50}
-            className={styles.logo}
-          />
-          <h1 className={styles.helpTitle}>Mail us</h1>
-          <p className={styles.helpText}>
-            Request a feature or Wanted to tell us your exprience.
-          </p>
-          <Link href="./help/contact" className={styles.gotoBtn} passHref>
-            Explore <span className={styles.arrow}>&#8594;</span>
-          </Link>
+
+        <article className={styles.contactCard}>
+          <div className={styles.imageDiv}>
+            <Image
+              src={contactLogo}
+              alt="Contact Logo"
+              className={styles.logo}
+            />
+          </div>
+          <div className={styles.content}>
+            <h1 className={styles.helpTitle}>Mail us</h1>
+            <p className={styles.helpText}>
+              Request a feature or Wanted to tell us your exprience.
+            </p>
+            <Link href="./help/contact" className={styles.gotoBtn} passHref>
+              Explore <span className={styles.arrow}>&#8594;</span>
+            </Link>
+          </div>
         </article>
-        <article className="contactCard">
-          <Image
-            src={issueLogo}
-            alt="Issue Logo"
-            width={50}
-            height={50}
-            className={styles.logo}
-          />
-          <h1 className={styles.helpTitle}>Report a bug</h1>
-          <p className={styles.helpText}>Found any error</p>
-          <Link
-            href="https://github.com/losier/bhagavadgitaapi/issues/new"
-            className={styles.gotoBtn}
-            passHref
-          >
-            Explore <span className={styles.arrow}>&#8594;</span>
-          </Link>
+
+        <article className={styles.contactCard}>
+          <div className={styles.imageDiv}>
+            <Image src={issueLogo} alt="Issue Logo" className={styles.logo} />
+          </div>
+          <div className={styles.content}>
+            <h1 className={styles.helpTitle}>Report a bug</h1>
+            <p className={styles.helpText}>Found any error</p>
+            <Link
+              href="https://github.com/losier/bhagavadgitaapi/issues/new"
+              className={styles.gotoBtn}
+              passHref
+            >
+              Explore <span className={styles.arrow}>&#8594;</span>
+            </Link>
+          </div>
         </article>
       </div>
     </div>
